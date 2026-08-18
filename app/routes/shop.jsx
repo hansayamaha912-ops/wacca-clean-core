77ff71c93f5481da3d3b423b5d787ce7f9399519
hospital-scraper <hansayamaha912@gmail.com>
2026-08-18 23:03:51 +0900

    0

--- app/routes/shop.jsx
+++ app/routes/shop.jsx
@@ -51,8 +51,8 @@ const PRODUCT_DATA = [
     description: "極度の潔癖症である男が、自らの手で作り上げた特製ガスマスク。マスクを通してしか心地良さを感じられない彼のアイテム",
     images: [
       "/assets/Gam2.PNG",
-      "/assets/Gam1.PNG",
-      "/assets/Gam3.PNG"
+      "/assets/Gam3.PNG",
+      "/assets/Gam1.PNG"
     ],
     sizes: ["ONE SIZE"],
     stripeUrl: null
